a1a59ce6c8829a31acb11960489a2bab8cfd6148
zwzhu <zwzhumail@163.com>
2014-06-27 20:25:33 +0800

    update

---
+++ src/com/inhuasoft/shsclient/Screens/MoreFragment.java
@@ -0,0 +1,20 @@
+package com.inhuasoft.shsclient.Screens;
+
+import com.inhuasoft.shsclient.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MoreFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.fragment_more, container, false);
+	}
+
+}
